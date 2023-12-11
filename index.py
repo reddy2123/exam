4d12457449a132cff198a390e4036d9cf56edfd3
Nandhan <reddynandhan2004@gmail.com>
2023-12-11 20:12:32 +0530

    first commit

---
+++ index.py
@@ -0,0 +1,12 @@
+import numpy as np
+array=np.array([3,4,5,6])
+print(array)
+sum=np.sum(array)
+print(sum)
+median=np.median(array)
+print(median)
+std=np.std(array)
+print(std)
+min=np.min(array)
+print(min)
+print(range)
